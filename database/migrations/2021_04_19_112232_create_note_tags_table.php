02af5f52df8e8cd0783bb01b96cff7049e1acb79
Nigel Croston <nige.csharp@gmail.com>
2021-04-20 11:52:30 +0100

    added migrations for initial DB and model setup Tag, NoteTag and Note

---
+++ database/migrations/2021_04_19_112232_create_note_tags_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNoteTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('note_tags', function (Blueprint $table) {
+            $table->integer('note_id')->unsigned();
+            $table->integer('tag_id')->unsigned();
+            $table->foreign('note_id')
+                ->references('id')
+                ->on('notes')
+                ->onDelete('cascade');
+            $table->foreign('tag_id')
+                ->references('id')
+                ->on('tags')
+                ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('note_tags');
+    }
+}
